fd9ce5afc8e455147fe70a47202f3cbc41963543
Siva-jothi <sivacs2004@gmail.com>
2026-03-12 15:44:00 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -148,6 +148,11 @@ resource "aws_instance" "jenkins_server" {
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+# add storage
+  root_block_device {
+    volume_size = 15
+    volume_type = "gp2"
+  }
 
  user_data = <<-EOF
 #!/bin/bash
